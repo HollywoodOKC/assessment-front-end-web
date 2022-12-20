e38228ddc32e04674751b08ff18710427157479d
HollywoodOKC <T_Lane11@yahoo.com>
2022-12-19 20:42:21 -0600

    Updated good changes.

--- about.js
+++ about.js
@@ -7,9 +7,11 @@ function handleSubmit(evt) {
 	alert(`User Successfully Submitted!`);
 }
 
-document.getElementById("ducky").addEventListener("mouseover", function () {
-	window.alert(`You're Doing a great job!`);
-});
+const myFunction = () => {
+	document.getElementById("ducky").addEventListener("mouseover", function () {
+		window.alert(`You're Doing a great job!`);
+	})
+}
 
 let form = document.querySelector('#contact');
 
